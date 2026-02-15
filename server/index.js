db723ce1d46055c03ccaa2a3427addadf4c8f45b
Valuehills Developer <developer@valuehills.com>
2026-02-15 08:54:26 -0800

    Fix CORS to allow production domain

--- server/index.js
+++ server/index.js
@@ -15,7 +15,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://valuehills.shop', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
